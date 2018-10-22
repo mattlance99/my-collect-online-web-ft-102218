4c951c5eb3db566d896d4d92476055041378021d
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-22 21:08:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,14 @@
-def my_collect(array)
-  
+def my_collect(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end 
